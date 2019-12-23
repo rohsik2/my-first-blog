14fba090d5d6d904a2bed6d1c161f629c4df72eb
눈큰왕눈이2 <rohsikdak@naver.com>
2019-12-23 11:00:18 +0000

    젠장 많이남았다구웃

--- blog/urls.py
+++ blog/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
-    path('calendar/', views.calendar, name='calendar')
+    path('calendar/', views.calendar, name='calendar'),
+    path('date/',views.left_day, name='left_day'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from .forms import PostForm
+from datetime import date
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
@@ -44,4 +45,11 @@ def post_edit(request, pk):
 
 def comment_list(request, post):
     comments = Comment.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+
+
+def left_day(request):
+    d_start = date(2019, 9, 2)
+    d_fin = date(2021,7,2)
+    d_delta = d_start-d_fin   
+    return render(request,'blog/left_day.html',{'leftday':d_delta})
 
