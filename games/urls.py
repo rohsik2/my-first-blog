ce5aae9caa340888fa13a82a63954142a77186bd
눈큰왕눈이2 <rohsikdak@naver.com>
2020-07-27 02:09:22 +0000

    죽은돌기능 추가

--- games/urls.py
+++ games/urls.py
@@ -4,5 +4,6 @@ from . import views
 from django.urls import path #include와 urls를 사용하기위해 import 해줘야 하는것
 
 urlpatterns = [
-    path('snake_game', views.snake_game, name='snake_game'),
+    path('go', views.go_game, name='go_game'),
+    path('', views.game_list, name='game_list'),
 ]
--- games/views.py
+++ games/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-def snake_game(request):
-    return render(request, 'games/snake_game.html')
+def go_game(request):
+    return render(request, 'games/go.html')
+
+def game_list(request):
+    return render(request, 'games/game_lists.html')
