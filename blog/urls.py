7f19fe7056b8063b7c1cc4386a5babbeeb6613a9
눈큰왕눈이2 <rohsikdak@naver.com>
2020-08-04 03:45:59 +0000

    남은날 우진이로 수정

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
-#from django.conf.urls import url
+# from django.conf.urls import url
 from . import views
-#from django.contrib import admin
-from django.urls import path #include와 urls를 사용하기위해 import 해줘야 하는것
+# from django.contrib import admin
+from django.urls import path  # include와 urls를 사용하기위해 import 해줘야 하는것
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
@@ -9,5 +9,6 @@ urlpatterns = [
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('calendar/', views.calendar, name='calendar'),
-    path('date/',views.left_day, name='left_day'),
+    path('date/', views.left_day, name='left_day'),
+
 ]
