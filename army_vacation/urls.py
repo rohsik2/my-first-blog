c4d091eec8bef88adb6c249c54e4f9e17e2f0270
눈큰왕눈이2 <rohsikdak@naver.com>
2020-08-08 07:18:24 +0000

    ascii error fix

--- army_vacation/urls.py
+++ army_vacation/urls.py
@@ -1,7 +1,7 @@
 # from django.conf.urls import url
 from . import views
 # from django.contrib import admin
-from django.urls import path  # include와 urls를 사용하기위해 import 해줘야 하는것
+from django.urls import path 
 
 
 urlpatterns = [
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include #include와 urls를 사용하기위해 import 해줘야 하는것
+from django.urls import path, include 
 
 
 urlpatterns = [
