546c9582634a5f5dfd5f6b2d63571f7d99340541
눈큰왕눈이2 <rohsikdak@naver.com>
2020-08-11 08:54:09 +0000

    add choice box

--- army_vacation/forms.py
+++ army_vacation/forms.py
@@ -3,7 +3,10 @@ from .models import Vacation
 
 
 class VacationForm(forms.ModelForm):
+    start_date = forms.DateField(help_text="ex) 12/31/2020");
+    end_date = forms.DateField(help_text="ex) 12/31/2020");
+    reason = forms.ChoiceField(choices=Vacation.SORT_OF_VACATIONS)
+    
     class Meta:
         model = Vacation
-        fields = ('reason', 'start_date', 'end_date',)
-
+        fields = ('start_date', 'end_date', 'reason')
--- army_vacation/models.py
+++ army_vacation/models.py
@@ -4,7 +4,16 @@ from django.db import models
 
 
 class Vacation(models.Model):
+    SORT_OF_VACATIONS = (
+        ('YG', '연가'),
+        ('PS', '포상휴가'),
+        ('WR', '위로휴가'),
+        ('CW', '청원휴가'),
+        )
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    reason = models.TextField()
+    reason = models.CharField(
+        max_length=2,
+        choices=SORT_OF_VACATIONS,
+        default='YG',)
     start_date = models.DateField()
     end_date = models.DateField()
