05146d5192d5735b440b0758e9c6d76a4098473b
눈큰왕눈이2 <rohsikdak@naver.com>
2020-09-03 06:18:36 +0000

    blog view bug fix

--- blog/views.py
+++ blog/views.py
@@ -27,7 +27,7 @@ def post_new(request):
            post = form.save(commit=False)
            post.published_date = timezone.now()
            post.save()
-           return redirect('post_detail', pk=post.pk)
+           return redirect('post_list')
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
@@ -42,7 +42,7 @@ def post_edit(request, pk):
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
-            return redirect('post_detail', pk=post.pk)
+            return redirect(post_list)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
